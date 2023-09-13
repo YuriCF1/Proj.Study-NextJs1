88a80f207c5e087a44d76fa8efa814ea9d07f770
Yuri Cruz <yuricruzf@gmail.com>
2023-09-13 06:45:23 -0300

    Starting first pages

---
+++ pages/FAQPage.js
@@ -0,0 +1,10 @@
+const FAQPage = () => {
+  return (
+    <>
+      <h1>Alura Case - Página de perguntas FAQ</h1>
+      <a href="/">Ir para a home</a>;
+    </>
+  );
+};
+
+export default FAQPage;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const index = () => {
+  return (
+    <>
+      <h1>Alura Case - Home page</h1>
+      <a href="/FAQPage">Ir para a página de perguntas</a>;
+    </>
+  );
+};
+
+export default index;
