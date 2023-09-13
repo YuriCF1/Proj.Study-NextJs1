3b362f3969baa85a585e0d840af8793962494042
Yuri Cruz <yuricruzf@gmail.com>
2023-09-13 07:40:33 -0300

    Testeing 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from "../src/components/Link";
+
+const index = () => {
+  return (
+    <>
+      <h1>Tá perdido?</h1>
+      {/* <Link href="/FAQPage">Ir para a página de perguntas</Link>; */}
+      <Link href={"/"}>Ir pra HOME</Link>
+    </>
+  );
+};
+
+export default index;
