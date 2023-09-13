7ba63420887e5b9afb375a83a80f6e2cbe29125c
Yuri Cruz <yuricruzf@gmail.com>
2023-09-13 07:39:47 -0300

    Using Link from Next

---
+++ src/components/Link/index.js
@@ -0,0 +1,12 @@
+import NextLink from "next/link";
+
+export default function Link({ children, href, ...props }) {
+  return (
+    <>
+      <NextLink href={href} {...props}>     {/* Props seria qualquer outra propriedade, como css */}
+        {children}
+      </NextLink>
+      ;
+    </>
+  );
+}
