cb6eac08337dc456d56b0404780a9370329f03ec
Yuri Cruz <yuricruzf@gmail.com>
2023-09-14 22:32:23 -0300

    Testing CSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import React from "react";
 import { useState } from "react";
+
 import Link from "../src/components/Link";
+
+//Modo padrão com module
+import styles from "./index.module.css"
+
+//Modo de estilização no next CSS-in-js
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>
+        {`
+          ${Tag} {
+            color: red;
+            font-family: sans-serif;
+          }
+        `}
+      </style>
+    </React.Fragment>
+  );
+}
 
 const index = () => {
   //Testando react
@@ -10,11 +33,12 @@ const index = () => {
 
   return (
     <>
-      <h1>Alura Case - Home page</h1>
+      <Title as="h1">Alura Case - Home</Title>
       {/* <Link href="/FAQPage">Ir para a página de perguntas</Link>; */}
       <Link href={"/FAQPage"}>Ir para a página do FAQ</Link>
       {/* <p>Contador: {contador}</p> */}
       {/* <button onClick={somandoContador}>clique</button> */}
+      <p className={styles.p}>Exemplo</p>
     </>
   );
 };
