05e96ce0a6a0110fd4beb2f090269e70e93938c5
Yuri Cruz <yuricruzf@gmail.com>
2023-09-14 21:41:49 -0300

    Testing reacT

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Link from "../src/components/Link";
 
 const index = () => {
+  //Testando react
+  // const [contador, setContador] = useState(0);
+  // const somandoContador = () => {
+  //   setContador(contador + 1);
+  // };
+
   return (
     <>
       <h1>Alura Case - Home page</h1>
       {/* <Link href="/FAQPage">Ir para a página de perguntas</Link>; */}
       <Link href={"/FAQPage"}>Ir para a página do FAQ</Link>
+      {/* <p>Contador: {contador}</p> */}
+      {/* <button onClick={somandoContador}>clique</button> */}
     </>
   );
 };
