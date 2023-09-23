07329b6ab0fd2b50449cd74a515212bf63ac762e
Yuri Cruz <yuricruzf@gmail.com>
2023-09-23 13:50:43 -0300

    Testing some configs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
 /**
  * @type {import('next').NextConfig}
  */
+
 const nextConfig = {
-    // output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+  // output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  trailingSlash: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/perguntas",
+        destination: "/faq",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
