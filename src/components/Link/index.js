5f73eaae23b6a5b3cf97abbd45640cc7a8d06421
Yuri Cruz <yuricruzf@gmail.com>
2023-09-25 00:07:35 -0300

    Cleaning

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -3,10 +3,9 @@ import NextLink from "next/link";
 export default function Link({ children, href, ...props }) {
   return (
     <>
-      <NextLink href={href} {...props}>     {/* Props seria qualquer outra propriedade, como css */}
+      <NextLink href={href} {...props}> {/* Props seria qualquer outra propriedade, como css */}
         {children}
       </NextLink>
-      ;
     </>
   );
 }
